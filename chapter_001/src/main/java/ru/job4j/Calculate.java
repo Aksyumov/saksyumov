c3f91d69cd5d70d20dc9dca8b8936b410a1e833c
Aksyumov <aksyumov@gmail.com>
2017-09-26 17:08:17 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j;
 
+/**
+ * Программа Hello Wolrd
+ * @author aksyumov
+ * @since 26.09.2017
+ * @version 1.0
+ */
 public class Calculate {
 	public static void main (String [] args) {
 	System.out.println ("Hello Wolrd!");
 	}
+	
+	/**
+	 * Вывод на печать "Hello World!"
+	 */
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Aksyumov Sergey (mailto:aksyumov@gmail.com)
+* @version 1.0
+* @since 26.09.2017
+*/
+package ru.job4j;
