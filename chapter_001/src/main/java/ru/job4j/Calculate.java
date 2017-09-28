7789f4c57b4a3647a1e2e82a0d0a623dcbbeaeab
Aksyumov <aksyumov@gmail.com>
2017-09-28 19:48:53 +0300

    Task #4122

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -8,8 +8,11 @@ package ru.job4j;
  */
 public class Calculate {
 	public static void main (String [] args) {
-	System.out.println ("Hello Wolrd!");
+	System.out.println ("Hello World!");
 	}
+	public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+}
 	
 	/**
 	 * Вывод на печать "Hello World!"
---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test
+ * @author aksyumov
+ * @since 28.09.2017
+ * @version 1.0
+ */
+public class CalculateTest {
+/**
+ * Test  echo.
+ */@ Test
+ public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Aksyumov";
+    String expect = "Echo, echo, echo : Aksyumov"; 
+    Calculate calc = new ru.job4j.Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+}
